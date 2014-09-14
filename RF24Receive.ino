ed5b00324b2753bc33b511475302b13ab7f3118e
contractorwolf <jameswolf@jamess-MacBook-Pro.local>
2014-09-14 18:10:39 -0400

    Major cleanup
    removed printf and all unnecessary code

--- RF24Receive.ino
+++ RF24Receive.ino
@@ -1,102 +1,57 @@
-/*
- Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
- */
-
-/**
- * Example for Getting Started with nRF24L01+ radios. 
- *
- * This is an example of how to use the RF24 class.  Write this sketch to two 
- * different nodes.  Put one of the nodes into 'transmit' mode by connecting 
- * with the serial monitor and sending a 'T'.  The ping node sends the current 
- * time to the pong node, which responds by sending the value back.  The ping 
- * node can then see how long the whole cycle took.
- */
 
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
-#include "printf.h"
-
-//
-// Hardware configuration
-//
 
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10 
-
 RF24 radio(9,10);
-
-//
-// Topology
-//
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
-//
-// Role management
-//
-// Set up role.  This sketch uses the same software for all the nodes
-// in this system.  Doing so greatly simplifies testing.  
-//
-
-
-
 void setup(void)
 {
-  //
-  // Print preamble
-  //
-
   Serial.begin(57600);
-  printf_begin();
-  printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
 
-  //
+  Serial.println();
+  Serial.println("RF24 Receive started");
+  Serial.println("*** PRESS 'T' to begin transmitting to the other node\n\r");
+
   // Setup and configure rf radio
-  //
-
   radio.begin();
 
   // optionally, increase the delay between retries & # of retries
   radio.setRetries(15,15);
 
-  // optionally, reduce the payload size.  seems to
-  // improve reliability
+  // optionally, reduce the payload size to improve reliability
   //radio.setPayloadSize(8);
 
-    radio.openWritingPipe(pipes[0]);
-    radio.openReadingPipe(1,pipes[1]);
-
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
 
   radio.startListening();
 
-  radio.printDetails();
+  radio.printDetails();//doesnt work without printf
 }
 
 void loop(void)
 {
-
     // if there is data ready
     if ( radio.available() )
     {
       // Dump the payloads until we've gotten everything
-      unsigned long got_time;
+      unsigned long got_message;
       bool done = false;
       while (!done)
       {
         // Fetch the payload, and see if this was the last one.
-        done = radio.read( &got_time, sizeof(unsigned long) );
+        done = radio.read( &got_message, sizeof(unsigned long) );
 
         // Spew it
-        printf("Got payload %lu...",got_time);
+        Serial.print("Got payload... ");
+        Serial.println(got_message);
 
-	// Delay just a little bit to let the other unit
-	// make the transition to receiver
+	// Delay to let the other unit transition to receiver
 	delay(20);
       }
 
@@ -104,12 +59,12 @@ void loop(void)
       radio.stopListening();
 
       // Send the final one back.
-      radio.write( &got_time, sizeof(unsigned long) );
-      printf("Sent response.\n\r");
-
+      radio.write( &got_message, sizeof(unsigned long) );
+      
       // Now, resume listening so we catch the next packets.
-      radio.startListening();
+      radio.startListening();      
+      
+      Serial.println("Response Sent, returned to listening");
     }
-
 }
 
